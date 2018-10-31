a7060fa7ad9e3cd11fdc6f63163b993c931ffa01
vvzvlad <vvzvlad@gmail.com>
2018-10-31 20:13:44 +0300

    Revert "Changed top header content to Rostelecom / Powered by Nokia."
    This reverts commit 76654cea3112a4a5cb4027495d21fbebb9ffab90.
    
    # Conflicts:
    #	src/assets/translation.json
    #	src/main_page.vue

--- src/main_page.vue
+++ src/main_page.vue
@@ -66,12 +66,12 @@
       <v-menu>
          <v-toolbar-title class="no-logo-margin"  slot="activator">
             <!-- <img class="mr-3 mt-2" src="./assets/logo_nokia.svg" height="25px" alt="Nokia Logo"> -->
-            {{$t("message.rostelecom_network_technologies")}}
+            {{$t("message.smart_city_control_center")}}
          </v-toolbar-title>
       </v-menu>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-         <div class="title mt-4" >{{$t("message.powered_by_nokia")}}</div>
+         <div class="title mt-4" >{{$t("message.powered_by_impact")}}</div>
       </v-toolbar-items>
    </v-toolbar>
 
@@ -83,7 +83,7 @@
 
 <script>
 import Vue from "vue";
-import Vue_i18n from 'vue-i18n';
+import Vue_i18n from "vue-i18n";
 import Vuex from "vuex";
 Vue.use(Vue_i18n, Vuex);
 
@@ -91,7 +91,7 @@ import languageSelect from "./common/language-select.vue";
 
 export default {
   components: {
-      languageSelect
+    languageSelect
   },
   data: () => ({
     menu_collapsed: true
@@ -109,260 +109,261 @@ export default {
     }
   },
   computed: {
-      items () { return [
+    items() {
+      return [
+        {
+          path: "dashboard",
+          text: this.$i18n.t("message.dashboard"),
+          icon: "fa-tachometer-alt"
+        },
+        {
+          path: "eco_monitoring",
+          text: this.$i18n.t("message.ecological_monitoring"),
+          icon: "fa-tree",
+          alticon: "fa-tree",
+          children: [
             {
-                path: "dashboard",
-                text: this.$i18n.t("message.dashboard"),
-                icon: "fa-tachometer-alt"
+              icon: "fa-map",
+              path: "/eco_monitoring/map",
+              text: this.$i18n.t("message.map")
             },
             {
-                path: "eco_monitoring",
-                text: this.$i18n.t("message.ecological_monitoring"),
-                icon: "fa-tree",
-                alticon: "fa-tree",
-                children: [
-                    {
-                        icon: "fa-map",
-                        path: "/eco_monitoring/map",
-                        text: this.$i18n.t("message.map")
-                    },
-                    {
-                        icon: "fa-th-list",
-                        path: "/eco_monitoring/stations",
-                        text: this.$i18n.t("message.stations")
-                    },
-                    /* {
+              icon: "fa-th-list",
+              path: "/eco_monitoring/stations",
+              text: this.$i18n.t("message.stations")
+            },
+            /* {
                       icon: "fa-calendar-alt",
                       path: "/eco_monitoring/history",
                       text: "Historical data"
                     }, */
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/eco_monitoring/aqi",
-                        text: this.$i18n.t("message.partner")+" #1"
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/eco_monitoring/sityair",
-                        text: this.$i18n.t("message.partner")+" #2"
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/eco_monitoring/aqi1",
-                        text: this.$i18n.t("message.partner")+" #3"
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/eco_monitoring/sityair1",
-                        text: this.$i18n.t("message.partner")+" #4"
-                    }
-                ]
+            {
+              icon: "fa-calendar-alt",
+              path: "/eco_monitoring/aqi",
+              text: this.$i18n.t("message.partner") + " #1"
             },
             {
-                path: "parking",
-                text: this.$i18n.t("message.parking"),
-                icon: "fa-car",
-                alticon: "fa-car",
-                children: [
-                    /* {
+              icon: "fa-calendar-alt",
+              path: "/eco_monitoring/sityair",
+              text: this.$i18n.t("message.partner") + " #2"
+            },
+            {
+              icon: "fa-calendar-alt",
+              path: "/eco_monitoring/aqi1",
+              text: this.$i18n.t("message.partner") + " #3"
+            },
+            {
+              icon: "fa-calendar-alt",
+              path: "/eco_monitoring/sityair1",
+              text: this.$i18n.t("message.partner") + " #4"
+            }
+          ]
+        },
+        {
+          path: "parking",
+          text: this.$i18n.t("message.parking"),
+          icon: "fa-car",
+          alticon: "fa-car",
+          children: [
+            /* {
                       icon: "fa-chart-line",
                       path: "/parking/stat",
                       text: "Statistic"
                     }, */
-                    {
-                        icon: "fa-map",
-                        path: "/parking/map",
-                        text: this.$i18n.t("message.map")
-                    },
-                    {
-                        icon: "fa-dot-circle",
-                        path: "/parking/partner1",
-                        text: this.$i18n.t("message.partner")+" #1"
-                    },
-                    {
-                        icon: "fa-dot-circle",
-                        path: "/parking/partner2",
-                        text: this.$i18n.t("message.partner")+" #2"
-                    }
-                    ,
-                    {
-                        icon: "fa-dot-circle",
-                        path: "/parking/partner3",
-                        text: this.$i18n.t("message.partner")+" #3"
-                    }
-                ]
+            {
+              icon: "fa-map",
+              path: "/parking/map",
+              text: this.$i18n.t("message.map")
             },
             {
-                path: "lighting",
-                text: this.$i18n.t("message.lighting"),
-                icon: "fa-lightbulb",
-                alticon: "fa-lightbulb",
-                children: [
-                     {
-                      icon: "fa-sliders-h",
-                      path: "/lighting/main",
-                      text: "Main"
-                    },
-                   /* {
+              icon: "fa-dot-circle",
+              path: "/parking/partner1",
+              text: this.$i18n.t("message.partner") + " #1"
+            },
+            {
+              icon: "fa-dot-circle",
+              path: "/parking/partner2",
+              text: this.$i18n.t("message.partner") + " #2"
+            },
+            {
+              icon: "fa-dot-circle",
+              path: "/parking/partner3",
+              text: this.$i18n.t("message.partner") + " #3"
+            }
+          ]
+        },
+        {
+          path: "lighting",
+          text: this.$i18n.t("message.lighting"),
+          icon: "fa-lightbulb",
+          alticon: "fa-lightbulb",
+          children: [
+            {
+              icon: "fa-sliders-h",
+              path: "/lighting/main",
+              text: "Main"
+            },
+            /* {
                       icon: "fa-chart-line",
                       path: "/lighting/energy_management",
                       text: "Management"
                     },*/
-                    {
-                        icon: "fa-external-link-square-alt",
-                        path: "/lighting/unilight",
-                        text: this.$i18n.t("message.partner")+" #1"
-                    },
-                    {
-                        icon: "fa-external-link-square-alt",
-                        path: "/lighting/unilight1",
-                        text: this.$i18n.t("message.partner")+" #2"
-                    },
-                    {
-                        icon: "fa-external-link-square-alt",
-                        path: "/lighting/unilight2",
-                        text: this.$i18n.t("message.partner")+" #3"
-                    },
-                    {
-                        icon: "fa-external-link-square-alt",
-                        path: "/lighting/unilight3",
-                        text: this.$i18n.t("message.partner")+" #4"
-                    }
-                ]
+            {
+              icon: "fa-external-link-square-alt",
+              path: "/lighting/unilight",
+              text: this.$i18n.t("message.partner") + " #1"
             },
             {
-                path: "safety",
-                text: this.$i18n.t("message.safety"),
-                icon: "fa-unlock-alt",
-                alticon: "fa-unlock-alt",
-                children: [
-                    /* {
+              icon: "fa-external-link-square-alt",
+              path: "/lighting/unilight1",
+              text: this.$i18n.t("message.partner") + " #2"
+            },
+            {
+              icon: "fa-external-link-square-alt",
+              path: "/lighting/unilight2",
+              text: this.$i18n.t("message.partner") + " #3"
+            },
+            {
+              icon: "fa-external-link-square-alt",
+              path: "/lighting/unilight3",
+              text: this.$i18n.t("message.partner") + " #4"
+            }
+          ]
+        },
+        {
+          path: "safety",
+          text: this.$i18n.t("message.safety"),
+          icon: "fa-unlock-alt",
+          alticon: "fa-unlock-alt",
+          children: [
+            /* {
                       icon: "fa-key",
                       path: "/safety/acs_lora",
                       text: "Access Control System"
                     }, */
-                    {
-                        icon: "fa-video",
-                        path: "/safety/video-analitics",
-                        text: this.$i18n.t("message.video_analytics"),
-                    },
-                    {
-                        icon: "fa-video",
-                        path: "/safety/video-monitoring",
-                        text: this.$i18n.t("message.video_monitoring"),
-                    }
-                ]
+            {
+              icon: "fa-video",
+              path: "/safety/video-analitics",
+              text: this.$i18n.t("message.video_analytics")
             },
             {
-                path: "metering",
-                text: this.$i18n.t("message.metering"),
-                icon: "fa-chart-bar",
-                alticon: "fa-chart-bar",
-                children: [
-                    {
-                        icon: "fa-sliders-h",
-                        path: "/metering/company",
-                        text: this.$i18n.t("message.management_company_page")
-                    },
-                    {
-                        icon: "fa-user",
-                        path: "/metering/personal",
-                        text: this.$i18n.t("message.personal_account")
-                    },
-                    {
-                        icon: "fa-compress",
-                        path: "/metering/meters",
-                        text: this.$i18n.t("message.meters")
-                    },
-                    {
-                        icon: "fa-external-link-square-alt",
-                        path: "/metering/linergo",
-                        text: this.$i18n.t("message.partner")+" #1"
-                    },
-                    {
-                        icon: "fa-external-link-square-alt",
-                        path: "/metering/gascloud",
-                        text: this.$i18n.t("message.partner")+" #2"
-                    },
-                    {
-                        icon: "fa-external-link-square-alt",
-                        path: "/metering/linergo1",
-                        text: this.$i18n.t("message.partner")+" #3"
-                    }
-                ]
+              icon: "fa-video",
+              path: "/safety/video-monitoring",
+              text: this.$i18n.t("message.video_monitoring")
+            }
+          ]
+        },
+        {
+          path: "metering",
+          text: this.$i18n.t("message.metering"),
+          icon: "fa-chart-bar",
+          alticon: "fa-chart-bar",
+          children: [
+            {
+              icon: "fa-sliders-h",
+              path: "/metering/company",
+              text: this.$i18n.t("message.management_company_page")
             },
             {
-                path: "infrastructure",
-                text: this.$i18n.t("message.infrastructure"),
-                icon: "fa-plug",
-                alticon: "fa-plug",
-                children: [
-                    {
-                        icon: "fa-chart-line",
-                        path: "/infrastructure/monitoring",
-                        text: this.$i18n.t("message.monitoring")
-                    },
-                    {
-                        icon: "fa-chart-line",
-                        path: "/infrastructure/gateways",
-                        text: this.$i18n.t("message.gateways")
-                    },
-                    /*           {
+              icon: "fa-user",
+              path: "/metering/personal",
+              text: this.$i18n.t("message.personal_account")
+            },
+            {
+              icon: "fa-compress",
+              path: "/metering/meters",
+              text: this.$i18n.t("message.meters")
+            },
+            {
+              icon: "fa-external-link-square-alt",
+              path: "/metering/linergo",
+              text: this.$i18n.t("message.partner") + " #1"
+            },
+            {
+              icon: "fa-external-link-square-alt",
+              path: "/metering/gascloud",
+              text: this.$i18n.t("message.partner") + " #2"
+            },
+            {
+              icon: "fa-external-link-square-alt",
+              path: "/metering/linergo1",
+              text: this.$i18n.t("message.partner") + " #3"
+            }
+          ]
+        },
+        {
+          path: "infrastructure",
+          text: this.$i18n.t("message.infrastructure"),
+          icon: "fa-plug",
+          alticon: "fa-plug",
+          children: [
+            {
+              icon: "fa-chart-line",
+              path: "/infrastructure/monitoring",
+              text: this.$i18n.t("message.monitoring")
+            },
+            {
+              icon: "fa-chart-line",
+              path: "/infrastructure/gateways",
+              text: this.$i18n.t("message.gateways")
+            },
+            /*           {
                       icon: "fa-chart-line",
                       path: "/infrastructure/managment",
                       text: "Management"
                     }, */
-                    {
-                        icon: "fa-exclamation-triangle",
-                        path: "/infrastructure/incidents",
-                        text: this.$i18n.t("message.incidents")
-                    }
-                ]
-            },
             {
-                path: "waste",
-                text: this.$i18n.t("message.waste"),
-                icon: "fa-trash-alt",
-                alticon: "fa-trash-alt",
-                children: [
-                    /* {
+              icon: "fa-exclamation-triangle",
+              path: "/infrastructure/incidents",
+              text: this.$i18n.t("message.incidents")
+            }
+          ]
+        },
+        {
+          path: "waste",
+          text: this.$i18n.t("message.waste"),
+          icon: "fa-trash-alt",
+          alticon: "fa-trash-alt",
+          children: [
+            /* {
                       icon: "fa-chart-line",
                       path: "/waste/stat",
                       text: "Statistic"
                     }, */
-                    {
-                        icon: "fa-th-list",
-                        path: "/waste/containers",
-                        text: this.$i18n.t("message.containers")
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/waste/planning",
-                        text: this.$i18n.t("message.planning")
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/waste/managment",
-                        text: this.$i18n.t("message.partner")+" #1"
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/waste/managment1",
-                        text: this.$i18n.t("message.partner")+" #2"
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/waste/managment2",
-                        text: this.$i18n.t("message.partner")+" #3"
-                    },
-                    {
-                        icon: "fa-calendar-alt",
-                        path: "/waste/managment3",
-                        text: this.$i18n.t("message.partner")+" #4"
-                    }
-                ]
+            {
+              icon: "fa-th-list",
+              path: "/waste/containers",
+              text: this.$i18n.t("message.containers")
+            },
+            {
+              icon: "fa-calendar-alt",
+              path: "/waste/planning",
+              text: this.$i18n.t("message.planning")
+            },
+            {
+              icon: "fa-calendar-alt",
+              path: "/waste/managment",
+              text: this.$i18n.t("message.partner") + " #1"
+            },
+            {
+              icon: "fa-calendar-alt",
+              path: "/waste/managment1",
+              text: this.$i18n.t("message.partner") + " #2"
+            },
+            {
+              icon: "fa-calendar-alt",
+              path: "/waste/managment2",
+              text: this.$i18n.t("message.partner") + " #3"
+            },
+            {
+              icon: "fa-calendar-alt",
+              path: "/waste/managment3",
+              text: this.$i18n.t("message.partner") + " #4"
             }
-        ]
-  }}
+          ]
+        }
+      ];
+    }
+  }
 };
 </script>
